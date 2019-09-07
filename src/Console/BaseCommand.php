af020c62ae0236fcee52fa79f18230776b18dcbb
Jordan Hoff <jhoff@twothirds.io>
2019-09-07 12:43:56 -0500

    Ignore class complexity

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -16,6 +16,9 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ */
 abstract class BaseCommand extends Command
 {
     /**
